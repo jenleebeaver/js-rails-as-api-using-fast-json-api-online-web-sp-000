8586df320950bebd075f8dc339649f3b306155f3
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-11-09 20:52:46 +0000

    Automatically backed up by Learn

--- app/serializers/sighting_serializer.rb
+++ app/serializers/sighting_serializer.rb
@@ -2,7 +2,12 @@
 class SightingSerializer
 
   include FastJsonapi::ObjectSerializer
-  #note we are also accessing related attributes from bird and location 
-  attributes :created_at, :bird, :location
+  #note we are also accessing related attributes from bird and location
+  attributes :created_at
+  #code below accesses related objects
+  #, :bird, :location
+
+  belongs_to :bird
+  belongs_to :location
 
 end
